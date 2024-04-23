64ebd4fad5d8e4da6205985935c8a907fbb1fc66
homayungit <homayunjust@gmail.com>
2024-04-23 09:12:31 +0600

    Commit All source

--- IDP/Models/ErrorViewModel.cs
+++ IDP/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-namespace IDP.Models
+namespace MagicVilla_Identity.Models
 {
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
